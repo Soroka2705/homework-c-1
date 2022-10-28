77bb48200b44851e704967f80380bb1793049c45
soroka <sorokindv88@mail.ru>
2022-10-28 19:34:50 +0300

    homework 1 saved

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = 5;
+int b = 7;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+
+Console.Write("max = ");
+Console.WriteLine($"{max}");
+
+
+
+
+Console.Write("input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (a > b) 
+{
+Console.WriteLine($"{a}"); 
+}
+else 
+{
+Console.WriteLine($"{b}"); 
+}
+
+
+
+
+Console.Write("input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (a > b)
+if (a > c)
+{
+    Console.WriteLine($"{a}"); 
+}
+if (b > a)
+if (b > c) 
+{
+    Console.WriteLine($"{b}");
+}
+else 
+{
+    Console.WriteLine($"{c}"); 
+}
+
+
+
+
+Console.Write("input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int div = a % 2;
+
+if (a % 2 == 1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
+
+
+Console.Write("input a positive integer number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int b = 2;
+
+while(b <= N)
+{
+    Console.Write(b + " ");
+    b += 2;
+}
+
+
+
+
+
